81a56b500a9cdc655d9a7746eec2faa8a92846e4
Ali Bin Naseer <aliisinsane0089@gmail.com>
2022-07-30 10:49:46 +0300

    Fixed Footer For Mobile V0.5.1

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer() {
 	return (
 		<footer className='relative bg-lightBlue bg-opacity-10'>
-			<div className='flex flex-col justify-center items-center h-[1.875rem] container mx-auto'>
-				<span className='font-poppinsregular text-[10px] tracking-wide text-[#000] text-center'>
+			<div className='flex flex-col justify-center items-center h-[1.875rem]'>
+				<span className='font-ubunturegular text-[10px] tracking-wide text-[#000] text-center'>
 					Ali Bin Naseer | All Rights Reserved
 				</span>
 			</div>
