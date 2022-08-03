37bdc61cf0bc4e838d9021bf9c75e79ecc7954f8
Ali Bin Naseer <aliisinsane0089@gmail.com>
2022-08-04 00:29:46 +0300

    404 Page Added V1.1.0

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<footer className='relative bg-lightBlue bg-opacity-10'>
+		<footer className='w-full bg-lightBlue bg-opacity-10'>
 			<div className='flex flex-col justify-center items-center h-[1.875rem] lg:h-[2.25rem]'>
 				<span className='font-ubunturegular text-[10px] lg:text-[12px] tracking-wide text-[#000] text-center'>
 					Ali Bin Naseer | All Rights Reserved
